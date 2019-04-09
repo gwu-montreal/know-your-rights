79eec5c13b917ee225770095435f5e98400870ff
gwuzyx <gwuzyx@users.noreply.github.com>
2019-04-08 22:51:39 -0400

    add web version

--- pandoc.sh
+++ pandoc.sh
@@ -5,3 +5,7 @@ for file in copy-src/*.md; do
     echo "$file -> ./copy-dest/$name.icml"
     pandoc "$file" -s -o "./copy-dest/$name.icml"
 done
+
+echo "writing text.md and who-are-we.md to html..."
+pandoc "./copy-src/text.md" "./copy-src/who-are-we.md" --template="web-template.html"\
+  -o "./docs/index.html" --metadata pagetitle="Know Your Rights!" -c "main.css"
